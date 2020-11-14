f0c7ebbcf934bf5280d85f6b698b21ea999c1ecd
pedrojsbezerra <pjsoares46@gmail.com>
2020-11-14 17:43:33 -0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var x = document.getElementById("latlong");
 function geo() {
     navigator.geolocation.getCurrentPosition(showPosition);
 }
 function showPosition(position) {
+  var x = document.getElementById("latlong");
   x.value = position.coords.latitude +","+position.coords.longitude;
 }
